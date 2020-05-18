df0034892c42a23a6f4b994e953ff88ea418e7c0
armnov <arm.nov@ya.ru>
2020-05-18 17:19:28 +0300

    build test ok

---
+++ app.c
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2010-2015 Intel Corporation
+ */
+
+/* typical launch: sudo ./build/app -l 1 -n 4 */
+
+#include <stdint.h>
+#include <inttypes.h>
+#include <rte_eal.h>
+#include <rte_ethdev.h>
+#include <rte_cycles.h>
+#include <rte_lcore.h>
+#include <rte_mbuf.h>
+
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
